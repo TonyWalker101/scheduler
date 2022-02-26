0a7fad452bfd3f38a0178868b3911bd3f67ae6a7
Tony <awalker780@gmail.com>
2022-02-26 14:17:58 -0500

    Refactor code via Refactoring Interview Components compass chapter

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -8,11 +8,10 @@ export default function InterviewerList(props) {
     return (
       <InterviewListItem 
         key={interviewer.id}
-        id={interviewer.id}
         name={interviewer.name}
         avatar={interviewer.avatar}
         selected={interviewer.id === props.interviewer}
-        setInterviewer={props.setInterviewer}
+        setInterviewer={() => props.setInterviewer(interviewer.id)}
       />)
       
   })
