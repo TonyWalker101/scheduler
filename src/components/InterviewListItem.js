4abde00d43d7ecda3826a0a2eb340229422dadf5
Tony <awalker780@gmail.com>
2022-02-26 12:03:55 -0500

    Refactor code to use generic data

--- src/components/InterviewListItem.js
+++ src/components/InterviewListItem.js
@@ -15,10 +15,10 @@ export default function InterviewListItem(props) {
     >
     <img
       className="interviewers__item-image"
-      src="https://i.imgur.com/LpaY82x.png"
-      alt="Sylvia Palmer"
+      src={props.avatar}
+      alt={props.name}
     />
-      {props.selected && 'Sylvia Palmer'}
+      {props.selected && props.name}
     </li>
   );
 }
