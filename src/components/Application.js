441200cab210ff0bc8d31cd96b7fee956ae2be7d
Tony <awalker780@gmail.com>
2022-03-06 13:24:14 -0500

    Include getInterviewerForDay function to App

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-import { getAppointmentsForDay, getInterview } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
 
 export default function Application(props) {
 
@@ -35,8 +35,10 @@ export default function Application(props) {
   const dailyAppointments = getAppointmentsForDay(state, state.day);
 
   const appointmentsArray = dailyAppointments.map(appointment => {
-
+    
     const interview = getInterview(state, appointment.interview);
+
+    const interviewers = getInterviewersForDay(state, state.day)
 
     return (
       <Appointment
@@ -44,6 +46,7 @@ export default function Application(props) {
       id={appointment.id}
       time={appointment.time}
       interview={interview}
+      interviewers={interviewers}
     />
     )
   });
