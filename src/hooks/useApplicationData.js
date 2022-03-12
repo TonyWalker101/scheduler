047facb294b8df387573c7b407a1a25c94b5c1f5
Tony <awalker780@gmail.com>
2022-03-11 20:03:52 -0500

    Create custom hook for storing App states

---
+++ src/hooks/useApplicationData.js
@@ -0,0 +1,64 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function useApplicationData() {
+
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {}
+  });
+
+  const setDay = day => setState({ ...state, day });
+
+  useEffect(() => {
+    const daysURL = `/api/days`;
+    const appointmentsURL = `/api/appointments`;
+    const interviewersURL = `/api/interviewers`;
+
+    Promise.all([
+      axios.get(daysURL),
+      axios.get(appointmentsURL),
+      axios.get(interviewersURL)
+    ]).then((all) => {
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
+    });
+
+  }, []);
+
+  function bookInterview(id, interview) {
+
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+
+    return axios.put(`/api/appointments/${id}`, {interview})
+    .then(setState({
+      ...state,
+      appointments
+    }));
+  }
+
+  function cancelInterview(id) {
+
+    return axios.delete(`/api/appointments/${id}`)
+    .then(setState({
+      ...state,
+    }));
+  }
+
+  return {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview
+  }
+
+}
