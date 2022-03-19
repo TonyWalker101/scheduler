60b13bc691d0d298e7dbd53c8603b31b959025a1
Tony <awalker780@gmail.com>
2022-03-19 15:54:55 -0400

    Remove getByTest from import

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup, fireEvent, getByText } from "@testing-library/react";
+import { render, cleanup, fireEvent } from "@testing-library/react";
 
 // import Appointment from "../Appointment/index";
 import Form from "../Appointment/Form";
